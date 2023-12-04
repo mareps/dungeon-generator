1ecd53cb4071054221665f2d003b70d6e078ed43
Marina <mareh.ps@gmail.com>
2023-12-03 22:35:46 -0300

    creating class Room

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,52 @@
-class Oi {
-    constructor() {
 
+interface Position {
+    x1: number, x2: number, y1: number, y2: number
+}
+interface RoomSize {
+    width: number, height: number
+}
+
+class Room {
+    private size: RoomSize;
+    private position: Position;
+
+    constructor() {
+        this.init();
     }
 
-    public oi(): void {
-        console.log('oi');
+    private init(): void {
+        this.setSize();
+        this.setPosition();
+        
+        console.log(this.getSize());
+        console.log(this.getPosition());
+    }
+
+    private setSize(): void {
+        // codigo para gerar tamanhos aleatorios dentro de uma base e range
+        this.size = {
+            width: 100,
+            height: 100
+        }
+    }
+
+    private setPosition(): void {
+        // codigo para gerar posicao aleatoria dentro do grid
+        this.position = {
+            x1: 0,
+            x2: 100,
+            y1: 0,
+            y2: 100
+        }
+    }
+
+    private getSize(): RoomSize {
+        return this.size;
+    }
+
+    private getPosition(): Position {
+        return this.position;
     }
 }
 
-const a = new Oi();
-a.oi();
+const firstRoom = new Room();
